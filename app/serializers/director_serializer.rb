b911deae92a190ab527bd7e99959880d5d26d20f
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-12-12 07:41:37 +0300

    Serializing  one to many association

--- app/serializers/director_serializer.rb
+++ app/serializers/director_serializer.rb
@@ -1,4 +1,6 @@
 class DirectorSerializer < ActiveModel::Serializer
   #Adding the desired attributes to the director_serializer 
   attributes :id, :name, :birthplace, :female_director
+  
+  has_many :movies
 end
